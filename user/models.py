e22d1c5047d9ab5455a9d9639d23ae2714754ff9
root <tech@hamwe.org>
2026-04-14 09:09:14 +0300

    static

--- user/models.py
+++ user/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from system.models import TimeStampMixin
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 
 class UserProfile(models.Model):
@@ -9,3 +12,10 @@ class UserProfile(models.Model):
 
     class Meta:
         db_table = 'user_profile'
+
+
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    print("SIGNAL FIRED")
+    if created:
+        Token.objects.create(user=instance)
--- user/views.py
+++ user/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.conf import settings
 
-# Create your views here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+
