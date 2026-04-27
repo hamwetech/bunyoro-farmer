a1091b27bdb04c7f58bbd5e92c6eed000edd5dc0
root <geoffrey.w.ndungu@gmail.com>
2026-04-27 13:04:33 +0000

    Update Training image

--- user/models.py
+++ user/models.py
@@ -41,18 +41,17 @@ class UserProfile(models.Model):
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance)
+        UserProfile.objects.get_or_create(user=instance)
 
 
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    if hasattr(instance, 'user'):
-        instance.user.save()
+# @receiver(post_save, sender=User)
+# def save_user_profile(sender, instance, **kwargs):
+#     if hasattr(instance, 'user'):
+#         instance.user.save()
 
 
 @receiver(post_save, sender=User)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
-    print("SIGNAL FIRED")
     if created:
         Token.objects.create(user=instance)
 
